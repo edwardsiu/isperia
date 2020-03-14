869498971f43ad3dd7467b7849abd44424a08e81
esiu <siu.edward.w@gmail.com>
2020-03-14 14:47:50 -0700

    archidekt connector

---
+++ server/src/services/deck_service/connectors/archidekt.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+const { URL } = require('url');
+
+function matches(url) {
+    const parsed = new URL(url);
+    return (parsed.hostname === 'archidekt.com') && (url.search(/\/decks\/\d+#/) > 0);
+}
+
+function toObject(obj, card) {
+    return Object.assign(obj, {
+        [card.card.oracleCard.name]: Number(card.quantity),
+    });
+}
+
+function extractDeckId(url) {
+    const result = url.match(/(?<=decks\/)\d+(?=#)/);
+    if (!result) {
+        throw new Error('Failed to extract deck id; invalid Archidekt url');
+    }
+    return result[0];
+}
+
+async function fetch(url) {
+    const deckId = extractDeckId(url);
+    const response = await axios.request({
+        method: 'GET',
+        url: `https://archidekt.com/api/decks/${deckId}`,
+    });
+    const cards = { response };
+    const commanders = cards.filter((card) => card.category === 'Commander').reduce(toObject, {});
+    if (commanders.length === 0) {
+        throw new Error('Not a commander decklist');
+    }
+    const decklist = cards.filter((card) => card.category !== 'Maybeboard').reduce(toObject, {});
+    return {
+        url,
+        commanders,
+        decklist,
+    };
+}
+
+module.exports = {
+    archidekt: {
+        fetch,
+        matches,
+    },
+};
--- server/src/services/deck_service/connectors/index.js
+++ server/src/services/deck_service/connectors/index.js
@@ -1,7 +1,9 @@
+const { archidekt } = require('./archidekt');
 const { scryfall } = require('./scryfall');
 const { tappedout } = require('./tappedout');
 
 module.exports = {
+    archidekt,
     scryfall,
     tappedout,
 };
