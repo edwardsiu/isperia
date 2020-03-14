984dfeeded046142bb6e4c9fc00a36fae371f45c
esiu <siu.edward.w@gmail.com>
2020-03-14 01:07:17 -0700

    isSupported helper

--- server/src/services/deck_service/deck_service.js
+++ server/src/services/deck_service/deck_service.js
@@ -1,10 +1,23 @@
 const connectors = require('./connectors');
 
+function findMatchingConnector(url) {
+    return Object.values(connectors).find((c) => c.matches(url));
+}
+
 async function fetch(url) {
-    const connector = Object.values(connectors).find((c) => c.matches(url));
+    const connector = findMatchingConnector(url);
+    if (!connector) {
+        throw new Error('Url not supported');
+    }
     return connector.fetch(url);
+}
+
+async function isSupported(url) {
+    const connector = Object.values(connectors).find((c) => c.matches(url));
+    return connector !== undefined;
 }
 
 module.exports = {
     fetch,
+    isSupported,
 };
